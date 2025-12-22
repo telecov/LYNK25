1dee563224d9da6d5c5e4478dcdec3df260f4e73
TELECOVIAJERO - CA2RDP <117606441+telecov@users.noreply.github.com>
2025-12-22 17:15:33 -0300

    Update about.php

--- about.php
+++ about.php
@@ -194,7 +194,7 @@
 <footer class="bg-dark text-white text-center py-3 mt-auto">
   🚀 Dashboard web LYNK25 Desarrollado por <strong>Telecoviajero - CA2RDP</strong> |
   <a href="https://github.com/telecov/LYNK25" target="_blank" class="text-info text-decoration-none">GitHub</a><br>
-    © <?php echo date('Y'); ?> Telecoviajero – CA2RDP.
+    2025 Telecoviajero – CA2RDP.
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
