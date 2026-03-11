b895b8e3d98fc3ba18078fd1c5527b88ea3b9445
TELECOVIAJERO - CA2RDP <117606441+telecov@users.noreply.github.com>
2026-03-10 22:52:29 -0300

    Delete js directory

--- js/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Si alguien intenta entrar directo a esta carpeta, lo mandamos al inicio
-header("Location: ../index.php");
-exit;
-
--- js/main.js
+++
@@ -1,6 +0,0 @@
-// Bootstrap tooltips
-document.addEventListener("DOMContentLoaded", function () {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-  tooltipTriggerList.map(function (el) { return new bootstrap.Tooltip(el); });
-});
-
